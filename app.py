458645e10c8fda41c130a086ad3cc1d813d90309
mateusaraujo3123 <LEIVISONMATEUS2021@GMAIL.COM>
2026-07-14 08:31:17 -0300

    Atualizar o app.py

--- app.py
+++ app.py
@@ -11,3 +11,11 @@ col1, col2, col3 = st.columns(3)
 with col1:
     if st.button("teste"):
        st.link_button("entra no YouTube", "HTTPS://www.youTube.com")
+
+with col2:
+    if st.button("FACEBOOK"):
+       st.link_button("entra no Facebook", "HTTPS://www.facebook.com")
+
+with col3:
+    if st.button("google"):
+       st.link_button("entra no Google", "HTTPS://www.google.com")
