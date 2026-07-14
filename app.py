bfb8dd878ca924fb0ccdf3514521468912513be6
mateusaraujo3123 <LEIVISONMATEUS2021@GMAIL.COM>
2026-07-14 11:02:31 -0300

    Update app.py

--- app.py
+++ app.py
@@ -10,12 +10,13 @@ col1, col2, col3 = st.columns(3)
 
 with col1:
     if st.button("teste"):
-       st.link_button("entra no YouTube", "HTTPS://www.youTube.com", use_container_width=True)
+       st.link_button("entra no YouTube", "HTTPS://www.youTube.com", use_container_width=True
+)
 
 with col2:
     if st.button("FACEBOOK"):
        st.link_button("entra no Facebook", "HTTPS://www.facebook.com", use_container_width=True
-                     )
+)
        
 with col3:
     if st.button("google"):
