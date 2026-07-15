7862651d6eb10726de387457aa53afd92319c339
mateusaraujo3123 <LEIVISONMATEUS2021@GMAIL.COM>
2026-07-15 13:37:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ with col2:
 with col3:
       st.link_button("LOVABLE", "https://www.remove.bg/pt-br/upload")
     
-st.markdown('<h1 class="sitesuteis">Sites Úteis</h1>', unsafe_allow_html=True)
+st.button('<h1 class="sitesuteis">Sites Úteis</h1>', unsafe_allow_html=True)
 st.markdown('<p class="facilidade">Sites de Facilidade</p>', unsafe_allow_html=True)
 
 col1, col2, col3 = st.columns(3)
