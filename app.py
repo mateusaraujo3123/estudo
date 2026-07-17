c59f946f03be7419a70641c2386cd9319bf2fbae
mateusaraujo3123 <LEIVISONMATEUS2021@GMAIL.COM>
2026-07-17 13:13:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.title("IA´s")
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    st.button("vefndas", "vendasssf")
+    st.link_button("vefndas", "https://www.magnific.com/premium-psd/young-woman-holding-car-keys-isolated-chroma-key-background-with-shocked-facial-expression_381046474.htm#fromView=search&page=1&position=16&uuid=1b9d5fef-2d1b-43cd-bf68-02aca95e739d&query=veiculo+mulher+segurando+chave")
 
 with col2:
     st.button("vendas", "vendasss")
