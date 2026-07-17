f52bef648d26c13a5b79f847a8cb4de11cf270b4
mateusaraujo3123 <LEIVISONMATEUS2021@GMAIL.COM>
2026-07-17 12:49:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ import streamlit as st
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    st.button("vendas")
+    st.button("vefndas", "vendasssf")
 
 with col2:
-    st.button("vendas")
+    st.button("vendas", "vendasss")
 
 with col3:
-    st.button("vendas")
+    st.button("vesndas", "vendasssdsfds")
