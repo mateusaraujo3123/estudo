9dc818671f941c311eb27f6da88977cb03e9128e
mateusaraujo3123 <LEIVISONMATEUS2021@GMAIL.COM>
2026-07-18 11:39:10 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,10 @@ st.button("IA´s")
 col1, col2, col3 = st.columns(3)
 
 with col1:
-<a href="https://chatgpt.com/" class="CHAT-GPT">
-chatgpt
-</a>
+    st.markdown("""
+    <a href="https://chatgpt.com/" class="CHAT-GPT">
+    chatgpt
+    </a>
 
 with col2:
     st.link_button("GOOGLE AI", "https://GOOGLE.COM")
