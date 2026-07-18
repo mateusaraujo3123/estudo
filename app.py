800094d76df1ae1904feb696393d023610ce05cf
mateusaraujo3123 <LEIVISONMATEUS2021@GMAIL.COM>
2026-07-18 11:41:06 -0300

    Fix formatting of chatgpt link in app.py

--- app.py
+++ app.py
@@ -7,9 +7,10 @@ col1, col2, col3 = st.columns(3)
 with col1:
     st.markdown("""
     <a href="https://chatgpt.com/" class="CHAT-GPT">
-    chatgpt
+      chatgpt
     </a>
-
+    """, unsafe_allow_html=True)
+    
 with col2:
     st.link_button("GOOGLE AI", "https://GOOGLE.COM")
 
