92c7a73586838fcc11a0fd7963730b230436cbcf
mateusaraujo3123 <LEIVISONMATEUS2021@GMAIL.COM>
2026-07-18 10:17:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ with col1:
     st.link_button("CHAT GPT", "https://chatgpt.com/")
 
 with col2:
-    st.button("GOOGLE AI", "https://GOOGLE.COM")
+    st.link_button("GOOGLE AI", "https://GOOGLE.COM")
 
 with col3:
-    st.button("LOVABLE", "https://lovable.dev/pt-br?utm_device=c&utm_source=google&utm_medium=paid_search_branded&utm_campaign=google-br-b2c-prospecting-evergreen-subscription-XB+-+Search+-+Lovable+-+CORE&campaignid=23350529747&gad_source=1&gad_campaignid=23350529747&gbraid=0AAAAA-iIxGcUPCFng6ocrYslRjYB7UTL6&gclid=EAIaIQobChMIhteW5qfclQMVpV9IAB0hNTJtEAAYASAAEgKepfD_BwE")
+    st.link_button("LOVABLE", "https://lovable.dev/pt-br?utm_device=c&utm_source=google&utm_medium=paid_search_branded&utm_campaign=google-br-b2c-prospecting-evergreen-subscription-XB+-+Search+-+Lovable+-+CORE&campaignid=23350529747&gad_source=1&gad_campaignid=23350529747&gbraid=0AAAAA-iIxGcUPCFng6ocrYslRjYB7UTL6&gclid=EAIaIQobChMIhteW5qfclQMVpV9IAB0hNTJtEAAYASAAEgKepfD_BwE")
