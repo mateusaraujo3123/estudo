6c8417a9d8204015cc24a4c43c058353d3f3cb4c
mateusaraujo3123 <LEIVISONMATEUS2021@GMAIL.COM>
2026-07-18 10:38:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -11,4 +11,4 @@ with col2:
     st.link_button("GOOGLE AI", "https://GOOGLE.COM")
 
 with col3:
-    st.link_button("LOVABLE", "https://lovable.dev/pt-br?utm_device=c&utm_source=google&utm_medium=paid_search_branded&utm_campaign=google-br-b2c-prospecting-evergreen-subscription-XB+-+Search+-+Lovable+-+CORE&campaignid=23350529747&gad_source=1&gad_campaignid=23350529747&gbraid=0AAAAA-iIxGcUPCFng6ocrYslRjYB7UTL6&gclid=EAIaIQobChMIhteW5qfclQMVpV9IAB0hNTJtEAAYASAAEgKepfD_BwE")
+    st.link_button("LOVABLE", "https://lovable.dev/")
